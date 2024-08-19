0d483dd3c4789ccac143db1083a75a72e79693e1
Iggyky <igrojas21@alumnos.utalca.cl>
2024-08-18 21:12:20 -0400

    clases protoboard y hoyito

---
+++ Project-e-Nigma/src/main/java/com/example/projectenigma/Protoboard.java
@@ -0,0 +1,49 @@
+package com.example.projectenigma;
+
+public class Protoboard {
+
+    //Atributos
+    Hoyito protoboard[][] = new Hoyito[14][30];
+
+    //Metodos
+
+    public void CambiarCargaBus(int Columna, int Fila){
+
+        switch (Fila){
+            case 0, 12:
+            {
+                //Positivo
+                for (int j = 0; j < Columna; j++){
+                    Hoyito hoyito = new Hoyito(12,12, true);
+                    protoboard[Fila][j] = hoyito;
+                }
+                break;
+            }
+            case 1, 13:
+            {
+                //Negativo
+                for (int j = 0; j < Columna; j++) {
+                    Hoyito hoyito = new Hoyito(12, 12, false);
+                    protoboard[Fila][j] = hoyito;
+                }
+                break;
+            }
+        }
+    }
+
+    public void CambiarCArgaHoyito(int Columna, int Fila){
+
+    }
+
+    public void EstadoHoyito(int Columna, int Fila){
+        System.out.println("Fila:"+ Fila + " Columna:"+ Columna );
+        System.out.println("Contenido:");
+        System.out.println("Cargavolt : "+ protoboard[Fila][Columna].getCargaVolt());
+        System.out.println("Cargaamp : "+ protoboard[Fila][Columna].getCargaAmp());
+        System.out.println("Estado : "+ protoboard[Fila][Columna].isEstado());
+
+    }
+
+
+
+}
