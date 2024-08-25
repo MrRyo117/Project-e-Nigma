318fa940880265bc92a81e1dc9deceb510fac40f
Iggyky <igrojas21@alumnos.utalca.cl>
2024-08-24 23:05:27 -0400

    cambios en el protoboard

--- Project-e-Nigma/src/main/java/com/example/projectenigma/SampleController.java
+++ Project-e-Nigma/src/main/java/com/example/projectenigma/SampleController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 public class SampleController {
 
@@ -13,13 +14,20 @@ public class SampleController {
     private Button iniciar;
     @FXML
     private AnchorPane AnchorPane;
+    @FXML
+    private Line linea;
 
     private Protoboard Protoboard2 = new Protoboard();
-    private Circle ArCircles[][] = new Circle[14][30];
+    private Circle[][] ArCircles = new Circle[14][30];
 
 
     @FXML
     protected void inicio() {
+
+        Protoboard2.CrearProtoboard(Protoboard2.protoboard.length,Protoboard2.protoboard[0].length);
+        Protoboard2.CambiarCargaBus(Protoboard2.protoboard.length,Protoboard2.protoboard[0].length);
+
+
         for(int i = 0; i < Protoboard2.protoboard.length; i++) {
             System.out.println("Fila: " + i);
 
@@ -32,10 +40,13 @@ public class SampleController {
             AnchorPane.getChildren().addAll(ArCircles[i]);
         }
 
-
-
     }
 
-
-
+    @FXML
+    public void imprimeline(){
+        System.out.println("linea posision end x: "+linea.getEndX());
+        System.out.println("linea posision end y: "+linea.getEndY());
+        System.out.println("linea posision start x: "+linea.getStartX());
+        System.out.println("linea posision start y: "+linea.getStartY());
+    }
 }
