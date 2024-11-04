ca3f6886406254239e66bff741828f0f072d33e2
MrRyo117 <marojas22@alumnos.utalca.cl>
2024-11-03 22:17:43 -0300

    cambio dibujo motor y captura de motor

--- Project-e-Nigma/src/main/java/com/example/projectenigma/Motor.java
+++ Project-e-Nigma/src/main/java/com/example/projectenigma/Motor.java
@@ -6,51 +6,9 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Line;
 
+/*
 public class Motor {
-    private Rectangle superficie;
-    private boolean motorEncendido;
-    private Circle boton_en_ap;
-    private Circle circulo_der;
-    private Circle circulo_izq;
-    private AnchorPane anchorPane;
 
-    public Motor(AnchorPane anchorPane) {
-        this.anchorPane=anchorPane;
-
-        superficie = new Rectangle();
-        superficie.setX(1150);
-        superficie.setY(450);
-        superficie.setHeight(80);
-        superficie.setWidth(100);
-        superficie.setFill(Color.rgb(43, 123, 228));
-        superficie.setStroke(Color.rgb(0, 0, 0));
-
-        boton_en_ap=new Circle();
-        boton_en_ap.setCenterX(1200);
-        boton_en_ap.setCenterY(490);
-        boton_en_ap.setRadius(12);
-        boton_en_ap.setFill(Color.rgb(224,60,28));
-        boton_en_ap.setStroke(Color.rgb(0,0,0));
-
-        circulo_der=new Circle();
-        circulo_der.setCenterX(1160);
-        circulo_der.setCenterY(540);
-        circulo_der.setRadius(12);
-        circulo_der.setFill(Color.rgb(43,123,228));
-        circulo_der.setStroke(Color.rgb(0,0,0));
-
-        circulo_izq=new Circle();
-        circulo_izq.setCenterX(1240);
-        circulo_izq.setCenterY(540);
-        circulo_izq.setRadius(12);
-        circulo_izq.setFill(Color.rgb(43,123,228));
-        circulo_izq.setStroke(Color.rgb(0,0,0));
-
-        motorEncendido=false;
-
-        boton_en_ap.setOnMouseClicked(mouseEvent -> Encender_Apagar());
-
-        anchorPane.getChildren().addAll(superficie,boton_en_ap,circulo_der,circulo_izq);
 
     }
 
@@ -84,4 +42,4 @@ public class Motor {
     public Circle getCirculo_izq(){
         return circulo_izq;
     }
-}
+}*/
